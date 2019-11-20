40e75bfebe4510d41f56fb100648fe54b006fc39
Ziqul <m.s.v.00a@gmail.com>
2019-11-20 14:29:23 +0200

    Successfully used graphics shaders to draw

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use vulkano::buffer::BufferUsage;
 use vulkano::buffer::CpuAccessibleBuffer;
 use vulkano::command_buffer::AutoCommandBufferBuilder;
 use vulkano::command_buffer::CommandBuffer;
+use vulkano::command_buffer::DynamicState;
 use vulkano::descriptor::descriptor_set::PersistentDescriptorSet;
 use vulkano::device::Device;
 use vulkano::device::DeviceExtensions;
@@ -13,62 +14,27 @@ use vulkano::device::Features;
 use vulkano::device::Queue;
 use vulkano::format::ClearValue;
 use vulkano::format::Format;
+use vulkano::framebuffer::Framebuffer;
+use vulkano::framebuffer::Subpass;
 use vulkano::image::Dimensions;
 use vulkano::image::StorageImage;
 use vulkano::instance::Instance;
 use vulkano::instance::InstanceExtensions;
 use vulkano::instance::PhysicalDevice;
 use vulkano::pipeline::ComputePipeline;
+use vulkano::pipeline::GraphicsPipeline;
+use vulkano::pipeline::viewport::Viewport;
 use vulkano::sync::GpuFuture;
 
+#[derive(Default, Copy, Clone)]
+struct Vertex {
+    position: [f32; 2],
+}
+vulkano::impl_vertex!(Vertex, position);
 
 fn main() -> Result<(), Box<Error>> {
 
     let (instance, device, queue) = init()?;
-
-    // Shading stuff [START]
-    mod cs {
-        vulkano_shaders::shader!{
-            ty: "compute",
-            src: "
-#version 450
-
-layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
-
-layout(set = 0, binding = 0, rgba8) uniform writeonly image2D img;
-
-void main() {
-    vec2 norm_coordinates = (gl_GlobalInvocationID.xy + vec2(0.5)) / vec2(imageSize(img));
-    vec2 c = (norm_coordinates - vec2(0.5)) * 2.0 - vec2(1.0, 0.0);
-
-    vec2 z = vec2(0.0, 0.0);
-    float i;
-    for (i = 0.0; i < 1.0; i += 0.005) {
-        z = vec2(
-            z.x * z.x - z.y * z.y + c.x,
-            z.y * z.x + z.x * z.y + c.y
-        );
-
-        if (length(z) > 4.0) {
-            break;
-        }
-    }
-
-    vec4 to_write = vec4(vec3(i), 1.0);
-    imageStore(img, ivec2(gl_GlobalInvocationID.xy), to_write);
-}"
-        }
-    }
-
-    let shader = cs::Shader::load(device.clone())?;
-
-    let compute_pipeline =
-        Arc::new(
-            ComputePipeline::new(
-                device.clone(), &shader.main_entry_point(), &()
-            )?
-        );
-    // Shading stuff [END]
 
     let image =
         StorageImage::new(
@@ -76,11 +42,88 @@ void main() {
             Format::R8G8B8A8Unorm, Some(queue.family())
         )?;
 
-    let set =
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+
+    let vertex_buffer =
+        CpuAccessibleBuffer::from_iter(
+            device.clone(), BufferUsage::all(),
+            vec![vertex1, vertex2, vertex3].into_iter()
+        )?;
+
+    mod vs {
+        vulkano_shaders::shader!{
+            ty: "vertex",
+            src: "
+#version 450
+
+layout(location = 0) in vec2 position;
+
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}"
+        }
+    }
+
+    mod fs {
+        vulkano_shaders::shader!{
+            ty: "fragment",
+            src: "
+#version 450
+
+layout(location = 0) out vec4 f_color;
+
+void main() {
+    f_color = vec4(1.0, 0.0, 0.0, 1.0);
+}"
+        }
+    }
+
+    let vs = vs::Shader::load(device.clone())?;
+    let fs = fs::Shader::load(device.clone())?;
+
+    let render_pass =
         Arc::new(
-            PersistentDescriptorSet::start(
-                compute_pipeline.clone(), 0
-            ).add_image(image.clone())?.build()?
+            vulkano::single_pass_renderpass!(
+                device.clone(),
+                attachments: {
+                    color: {
+                        load: Clear,
+                        store: Store,
+                        format: Format::R8G8B8A8Unorm,
+                        samples: 1,
+                    }
+                },
+                pass: {
+                    color: [color],
+                    depth_stencil: {}
+                }
+            )?
+        );
+
+    let pipeline =
+        Arc::new(
+            GraphicsPipeline::start()
+                // Defines what kind of vertex input is expected.
+                .vertex_input_single_buffer::<Vertex>()
+                // The vertex shader.
+                .vertex_shader(vs.main_entry_point(), ())
+                // Defines the viewport.
+                .viewports_dynamic_scissors_irrelevant(1)
+                // The fragment shader.
+                .fragment_shader(fs.main_entry_point(), ())
+                // This graphics pipeline object concerns the first pass of the render pass.
+                .render_pass(Subpass::from(render_pass.clone(), 0).unwrap())
+                // Now that everything is specified, we call `build`.
+                .build(device.clone())?
+        );
+
+    let framebuffer =
+        Arc::new(
+            Framebuffer::start(render_pass.clone())
+                .add(image.clone())?
+                .build()?
         );
 
     let buf =
@@ -89,18 +132,32 @@ void main() {
             (0 .. 1024 * 1024 * 4).map(|_| 0u8)
         )?;
 
+    let dynamic_state =
+        DynamicState {
+            viewports: Some(vec![Viewport {
+                origin: [0.0, 0.0],
+                dimensions: [1024.0, 1024.0],
+                depth_range: 0.0 .. 1.0,
+            }]),
+            .. DynamicState::none()
+        };
+
     let command_buffer =
-        AutoCommandBufferBuilder::new(device.clone(), queue.family())?
-            .dispatch([1024 / 8, 1024 / 8, 1], compute_pipeline.clone(), set.clone(), ())?
+        AutoCommandBufferBuilder::primary_one_time_submit(
+            device.clone(), queue.family()
+        )?
+            .begin_render_pass(framebuffer.clone(), false, vec![[0.0, 0.0, 1.0, 1.0].into()])?
+            .draw(pipeline.clone(), &dynamic_state, vertex_buffer.clone(), (), ())?
+            .end_render_pass()?
             .copy_image_to_buffer(image.clone(), buf.clone())?
-            .build()?;
+            .build()?;;
 
     let finished = command_buffer.execute(queue.clone())?;
     finished.then_signal_fence_and_flush()?.wait(None)?;
 
     let buffer_content = buf.read()?;
     let image = ImageBuffer::<Rgba<u8>, _>::from_raw(1024, 1024, &buffer_content[..]).unwrap();
-    image.save("image.png")?;
+    image.save("triangle.png")?;
 
     Ok(())
 }
